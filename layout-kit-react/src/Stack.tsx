af4ceebeacb559276be50a1295e4a7f7ef022169
remorses <beats.by.morse@gmail.com>
2020-06-09 12:53:06 +0200

    layout-kit-react/src/Stack: default is stretch

--- layout-kit-react/src/Stack.tsx
+++ layout-kit-react/src/Stack.tsx
@@ -43,7 +43,7 @@ export const Stack: FC<StackProps> = forwardRef(
     (
         {
             direction = 'column',
-            align,
+            align = 'stretch',
             justify,
             spacing = '0px',
             children,
